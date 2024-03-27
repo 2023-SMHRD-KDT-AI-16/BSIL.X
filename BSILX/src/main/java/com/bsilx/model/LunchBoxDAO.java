073c821a7dcb61af5fe962d045fcabebbc3420ce
god2_016 <god2_016@daum.net>
2024-03-27 21:22:15 +0900

    해시태그 기능 and로 구현 - 시은

--- BSILX/src/main/java/com/bsilx/model/LunchBoxDAO.java
+++ BSILX/src/main/java/com/bsilx/model/LunchBoxDAO.java
@@ -1,5 +1,6 @@
 package com.bsilx.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -10,30 +11,76 @@ import com.bsilx.db.SqlSessionManager;
 public class LunchBoxDAO {
 
 	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
-	
-	// 특정 재료 포함된 레시피 출력(해시태그)
-	public List<LunchBoxDTO> selectLbox (List<String> ingre_name) {
+
+	// 특정 재료 포함된 레시피 이름 리스트에 담는 메소드(해시태그)
+	public List<String> selectLboxName(String ingre_name) {
 
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 
-		List<LunchBoxDTO> priceList = null;
-
+		List<String> LboxNameList = null;
+		System.out.println("검색할 식재료 : " + ingre_name);
 		try {
-			priceList = sqlSession.selectList("SelectLbox", ingre_name);
-			System.out.println("리스트 담기 성공");
+			LboxNameList = sqlSession.selectList("SelectLboxName", ingre_name);
+			System.out.println("레시피 출력 성공");
+			System.out.println(LboxNameList);
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.out.println("리스트 담기 실패");
+			System.out.println("레시피 출력 실패");
 		} finally {
 			sqlSession.close();
 		}
 
-		return priceList;
+		return LboxNameList;
 	}
-	
+
+	// 해시태그 기능 메인 메소드
+	public List<LunchBoxDTO> selectLbox(List<String> LboxNameList) {
+
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
+		List<String> LboxList = new ArrayList<>();
+
+		for (String ingredient : LboxNameList) {
+
+			List<String> tempList = new LunchBoxDAO().selectLboxName(ingredient);
+
+			if (LboxList.isEmpty()) {
+
+				LboxList.addAll(tempList);
+
+				System.out.println("첫 번째 음식 : " + ingredient);
+				System.out.println("LboxList1 :" + LboxList);
+
+			} else {
+				LboxList.retainAll(tempList);
+				System.out.println("두 번째 이후의 음식: " + ingredient);
+			}
+		}
+		System.out.println("LboxList2 :" + LboxList);
+
+		List<LunchBoxDTO> LunchBoxList = null;
+
+		try {
+			LunchBoxList = sqlSession.selectList("SelectLbox", LboxList);
+			if (LunchBoxList != null) {
+				System.out.println("해시태그 담기 성공");
+			} else {
+				System.out.println("해시태그 담기 실패");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("통신 실패");
+		} finally {
+			sqlSession.close();
+		}
+
+		return LunchBoxList;
+
+	}
+
 	// 클릭한 레시피 정보 가져오는 메소드
-	public LunchBoxDTO selectOneLbox (String lbox_name) {
-		
+	public LunchBoxDTO selectOneLbox(String lbox_name) {
+
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 		LunchBoxDTO lbox = null;
 		try {
@@ -45,17 +92,17 @@ public class LunchBoxDAO {
 		} finally {
 			sqlSession.close();
 		}
-		
+
 		return lbox;
 	}
-	
+
 	// 클릭한 레시피 재료 가져오는 메소드
-	public List<IngrePriceDTO> selectLboxIngre (String lbox_name) {
-		
+	public List<IngrePriceDTO> selectLboxIngre(String lbox_name) {
+
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 
 		List<IngrePriceDTO> lboxIngreList = null;
-		
+
 		try {
 			lboxIngreList = sqlSession.selectList("SelectOneLboxIngre", lbox_name);
 			System.out.println("클릭한 메뉴 레시피 출력 성공");
@@ -65,10 +112,8 @@ public class LunchBoxDAO {
 		} finally {
 			sqlSession.close();
 		}
-		
+
 		return lboxIngreList;
 	}
-	
-	
-	
+
 }
