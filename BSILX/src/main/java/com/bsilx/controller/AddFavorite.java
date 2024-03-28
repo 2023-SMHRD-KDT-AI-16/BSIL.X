0e1c27982ad279f1ab2d88461fc8c06f993cd495
god2_016 <god2_016@daum.net>
2024-03-28 15:13:37 +0900

    즐겨찾기 기능 구현

---
+++ BSILX/src/main/java/com/bsilx/controller/AddFavorite.java
@@ -0,0 +1,56 @@
+package com.bsilx.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.bsilx.model.BookmarkDTO;
+import com.bsilx.model.MemberDAO;
+
+public class AddFavorite extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+
+		int lbox_seq = (int) session.getAttribute("lbox_seq");
+
+		String user_id = (String) session.getAttribute("user_id");
+
+		BookmarkDTO mdto = new BookmarkDTO(lbox_seq, user_id);
+		
+		MemberDAO dao = new MemberDAO();
+
+		// 로그인 X
+		if (user_id == null) {
+			response.getWriter().write("loginFirst");
+
+		// 즐겨찾기 되어있을 때
+		} else if (dao.selectBookmark(mdto) != null) {
+			int result = dao.deleteBookmark(mdto);
+			if (result > 0) {
+				response.getWriter().write("delete");
+			} else {
+				System.out.println("즐겨찾기 삭제 오류");
+			}
+
+		} else {
+
+			int result = dao.insertBookmark(mdto);
+			if (result > 0) {
+				response.getWriter().write("add");
+			} else {
+				System.out.println("즐겨찾기 추가 오류");
+			}
+		}
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("UTF-8");
+
+	}
+
+}
---
+++ BSILX/src/main/java/com/bsilx/controller/CheckFavorites.java
@@ -0,0 +1,42 @@
+package com.bsilx.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.bsilx.model.BookmarkDTO;
+import com.bsilx.model.MemberDAO;
+
+public class CheckFavorites extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		
+		int lbox_seq = (int)session.getAttribute("lbox_seq");
+		
+		String user_id = (String)session.getAttribute("user_id");
+		
+		BookmarkDTO mdto = new BookmarkDTO(lbox_seq, user_id);
+		
+		MemberDAO dao = new MemberDAO();
+		
+		String result = dao.selectBookmark(mdto);
+		
+		if(result != null) {
+			response.getWriter().write("existFavorite");
+			
+		}
+			response.setContentType("text/plain");
+			response.setCharacterEncoding("UTF-8");
+		}
+		
+		
+}
